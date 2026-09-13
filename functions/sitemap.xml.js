86caebdaa1436eecc01f7ac914d346cccd523601
WHALESLAUGHTER <whaleslaughter@gmail.com>
2026-09-12 22:00:39 -0400

    Add image support to sitemap generation

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -17,7 +17,8 @@ export async function onRequest(context) {
   ];
 
   let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
-  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"';
+  xml += ' xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n';
 
   for (const page of staticPages) {
     xml += '  <url>\n';
@@ -32,6 +33,15 @@ export async function onRequest(context) {
     xml += `    <loc>${base}/article.html?id=${article.id}</loc>\n`;
     xml += `    <lastmod>${article.date}</lastmod>\n`;
     xml += `    <priority>0.8</priority>\n`;
+    if (article.img) {
+      const imgUrl = article.img.startsWith('http') ? article.img : base + article.img;
+      const caption = escXml(article.alt || article.title);
+      xml += '    <image:image>\n';
+      xml += `      <image:loc>${escXml(imgUrl)}</image:loc>\n`;
+      xml += `      <image:caption>${caption}</image:caption>\n`;
+      xml += `      <image:title>${escXml(article.title)}</image:title>\n`;
+      xml += '    </image:image>\n';
+    }
     xml += '  </url>\n';
   }
 
@@ -40,4 +50,13 @@ export async function onRequest(context) {
   return new Response(xml, {
     headers: { 'Content-Type': 'application/xml' },
   });
+}
+
+function escXml(s) {
+  return String(s)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
 }
